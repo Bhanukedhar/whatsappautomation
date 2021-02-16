2c217d315610383345a865d0bbe2143dc137a748
Bhanu <34902129+Bhanukedhar@users.noreply.github.com>
2021-02-16 06:34:54 +0530

    Update whatsapptwilio.py

--- whatsapptwilio.py
+++ whatsapptwilio.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC9b88b0850dd83e0cc32885a4510af590'
-auth_token = '7c3a5613003065134757e7fb9eea846d'
+account_sid = 'AUTH_SID'
+auth_token = 'AUTH_KEY'
 client = Client(account_sid, auth_token)
 
 
